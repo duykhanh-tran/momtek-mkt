bb36fb28ec0cb76dc07e61074190dcf52637d41d
Duy_Beo <duy171534@gmail.com>
2025-12-16 15:49:14 +0700

    Fix Admin Panel white screen

--- backend/src/payload.config.ts
+++ backend/src/payload.config.ts
@@ -29,6 +29,7 @@ const dirname = path.dirname(filename);
 dotenv.config({ path: path.resolve(dirname, '../.env') });
 
 export default buildConfig({
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL || '',
   admin: {
     user: Users.slug, 
   },
